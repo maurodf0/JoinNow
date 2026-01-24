d781f19e82036dd490f0b602907f80a7ecd52b3d
Mauro <maurodefalco96@gmail.com>
2026-01-24 23:11:12 +0100

    Aggiungi componenti della tabella e implementa la visualizzazione degli utenti registrati

--- app/pages/data.vue
+++ app/pages/data.vue
@@ -1,12 +1,49 @@
 <script setup lang="ts">
 
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
   const response = await $fetch('/api/data');
   const data = await response;
+
+  const users = data.body;
 
 </script>
 
 <template>
   <div>
-    <pre>{{ data }}</pre>
+    <Table>
+    <TableCaption>Logged in Users</TableCaption>
+    <TableHeader>
+      <TableRow>
+        <TableHead class="w-[100px]">
+          ID
+        </TableHead>
+        <TableHead>Time</TableHead>
+        <TableHead>Name</TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <template v-for="{ id, time, name } in users">
+          <TableRow>
+          <TableCell class="font-medium">
+            {{ id }}
+          </TableCell>
+          <TableCell>{{ time }}</TableCell>
+          <TableCell>{{ name }}</TableCell>
+        </TableRow>
+        </template>
+    
+
+    </TableBody>
+  </Table>
   </div>
 </template>
