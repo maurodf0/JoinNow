8a83cc68ce444579960210f351dce0856c61f2d9
Mauro <maurodefalco96@gmail.com>
2026-01-31 00:42:04 +0100

    Remove redundant addAdmin function definition and update dialog submission to directly call addAdmin without parameters for cleaner admin addition process.

--- app/pages/profile.vue
+++ app/pages/profile.vue
@@ -33,9 +33,6 @@ const addAdmin = async (name: string, role: string) => {
    })
 }
 
-const addAdmin = (role: string, name: string) => {
-    console.log(role, name)
-}
 </script>
 
 <template>
@@ -48,12 +45,12 @@ const addAdmin = (role: string, name: string) => {
         <div v-if="user?.user_metadata?.role === 'admin'" class="mt-4">
             <h2 class="text-2xl font-bold">Admin</h2>
             <p>Add another Admin</p>
-            <Button @click="addAdmin" class="cursor-pointer mt-4">Data</Button>
+            
         </div>
 
         <DialogButton 
         title="Add another Admin" 
         description="Add another Admin to the system" 
-        buttonText="Add Admin" @dialogSubmit="addAdmin($event)" />
+        buttonText="Add Admin" @dialogSubmit="addAdmin" />
     </Container>
 </template>
