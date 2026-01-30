f354fd54c433c905e78bd31a59e1a83a8909576c
Mauro <maurodefalco96@gmail.com>
2026-01-31 00:31:39 +0100

    Add role selection to DialogButton component; implement form submission handling for user role and name input.

--- app/components/site/DialogButton.vue
+++ app/components/site/DialogButton.vue
@@ -11,7 +11,26 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 
+
+const roles = [
+  { value: 'admin', label: 'Admin' },
+  { value: 'user', label: 'User' },
+  { value: 'developer', label: 'Developer' },
+  { value: 'worker', label: 'Worker' },
+  { value: 'other', label: 'Other' },
+]
+
+defineEmits<{
+  (e: 'submit', role: string, name: string): void
+}>()
+
+const onSubmit = (role: string, name: string) => {
+    emit('submit', role, name)
+}
+
+const role = ref<string>('')
 
 
 const props = defineProps<{
@@ -23,7 +42,7 @@ const props = defineProps<{
 
 <template>
   <Dialog>
-    <form>
+    <form @submit.prevent="onSubmit(role, name)">
       <DialogTrigger as-child>
         <Button variant="outline">
           {{ props.buttonText }}
@@ -37,11 +56,20 @@ const props = defineProps<{
         <div class="grid gap-4">
           <div class="grid gap-3">
             <label for="name-1" class="text-sm font-medium leading-none">Name</label>
-            <Input id="name-1" name="name" default-value="Pedro Duarte" />
+            <Input id="name-1" name="name"  />
           </div>
           <div class="grid gap-3">
-            <label for="username-1" class="text-sm font-medium leading-none">Username</label>
-            <Input id="username-1" name="username" default-value="@peduarte" />
+            <label for="username-1" class="text-sm font-medium leading-none">Role</label>
+            <Select id="username-1" name="username" v-model="role">
+            <SelectTrigger>
+              <SelectValue placeholder="Select a role" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem v-for="role in roles" :key="role.value" :value="role.value">
+                <span>{{ role.label }}</span>
+              </SelectItem>
+            </SelectContent>
+          </Select>
           </div>
         </div>
         <DialogFooter>
