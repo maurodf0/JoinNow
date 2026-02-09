870a423dc173a92b69a7436cdb40c8d9624a2739
Mauro <maurodefalco96@gmail.com>
2026-02-10 00:00:01 +0100

    chore: Remove unused data retrieval and deletion API endpoints to streamline server code and improve maintainability.

---
+++ server/api/data/[id].get.ts
@@ -0,0 +1,37 @@
+import { serverSupabaseClient } from '#supabase/server'
+import { serverSupabaseUser } from '#supabase/server'
+
+export default defineEventHandler(async (event) => {
+  const supabase = await serverSupabaseClient(event)
+  const user = await serverSupabaseUser(event);
+
+  // Get query take the GET value from the request
+  const page = Number(getQuery(event).page) || 1;
+  const limit = Number(getQuery(event).limit) || 10;
+
+  // Calculate the range of the query
+  const from = (page - 1) * limit;
+  const to = from + limit - 1;
+
+  if(user.user_metadata.role !== 'admin') return;
+  const { data, error, count } = await supabase
+    .from('Joiners')
+    
+    .select('*', { count: 'exact' })
+    .order('created_at', { ascending: false })
+    .range(from, to)
+
+  if (error) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: error.message
+    })
+  }
+
+  return {
+    body: data,
+    total: count
+  };
+});
+
+
