c8aabacd08f2472d1594b175024aaf9cf5837616
Mauro <maurodefalco96@gmail.com>
2026-02-09 23:54:14 +0100

    feat: Add user role validation for delete operation to restrict access to admin users only

--- server/api/data.delete.ts
+++ server/api/data.delete.ts
@@ -1,7 +1,11 @@
 import { serverSupabaseClient } from '#supabase/server'
+import { serverSupabaseUser } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
     const supabase = await serverSupabaseClient(event)
+    const user = await serverSupabaseUser(event);
+
+    if(user?.user_metadata?.role !== 'admin') return;
     const { id } = await readBody(event)
     const { data, error } = await supabase
         .from('Joiners')
